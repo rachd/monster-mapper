37713a20da88604c18545deb5898ff10c9ee5281
Jorge Vargas <jvargas2@icloud.com>
2016-07-09 20:59:25 -0400

    Set up full project
    Had to set up the full project without the former commits due to Pods
    being too large for Github.

---
+++ MonsterMappers/MonsterMappers/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  MonsterMappers
+//
+//  Created by Jorge Vargas on 7/9/16.
+//  Copyright © 2016 Monstographers. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
